04fb87a32ee67022d1d7625ef109270509ea3d6c
O.R <orezek@seznam.cz>
2024-12-12 20:59:45 +0100

    Implement: little playerinfo service done

--- srcs/auth_service/auth_service.rb
+++ srcs/auth_service/auth_service.rb
@@ -1,23 +1,34 @@
-5.times do
-	puts "Hello World"
+require 'sinatra'
+require 'json'
+
+# Mock database of players
+PLAYERS = {
+  1 => { id: 1, name: 'm_sulc', score: 1200 },
+  2 => { id: 2, name: 'm_bartos', score: 201 }
+}
+
+# Get player information
+get '/api/auth/player-info' do
+  # Parse the player ID from query parameters
+  player_id = params['id']&.to_i
+
+  # Lookup the player in the mock database
+  player = PLAYERS[player_id]
+
+  if player
+    # Player found, return as JSON
+    content_type :json
+    status 200
+    player.to_json
+  else
+    # Player not found
+    content_type :json
+    status 404
+    { error: 'Player not found' }.to_json
+  end
 end
 
-for i in 1..5
-	puts "Another Hello World"
-end
-
-count = 0
-while count <= 5
-	puts "Yet Another Hello World"
-	count += 1
-end
-
-count = 0
-until count == 5
-	puts "Ohh, Hello World"
-	count += 1
-end
-
-for k in 1..3
-	puts "Who run the container first? Secret is: sahava."
+# Welcome route
+get '/' do
+  { message: "Welcome to Transcendence" }.to_json
 end
