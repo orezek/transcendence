fc92d521d1f73d0c6eafeafc467f428108469b4c
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-12-18 13:37:05 +0100

    feat(docker): reformat db_service

--- srcs/db_service/init/02_create_indexes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Creating additional indexes..."
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE INDEX idx_users_email ON users(email);
-EOSQL
-
