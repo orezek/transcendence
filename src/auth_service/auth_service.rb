66fe867866a408f7ece4adad2241f190f251c182
O.R <orezek@seznam.cz>
2024-12-21 21:39:29 +0100

    refactor: removing unused code or debug code

--- src/auth_service/auth_service.rb
+++ src/auth_service/auth_service.rb
@@ -3,7 +3,8 @@ require 'json'
 require 'pg'
 require 'sequel'
 require 'bcrypt'
-require_relative 'jwt_manager'
+require './jwt_manager'
+require './db_handler'
 
 class AuthService < Sinatra::Base
   set :bind, '0.0.0.0'
@@ -29,32 +30,8 @@ class AuthService < Sinatra::Base
     @current_user = User.where(id: decoded_token['user_id']).first
     halt 401, { error: 'Unauthorized' }.to_json unless @current_user
   end
-
-
-  db_connection = {
-    adapter: 'postgres',
-    host: 'db-service',
-    database: 'auth_db',
-    user: 'auth_user',
-    password: 'securepassword'
-  }
-  DB = Sequel.connect(db_connection)
-
-  # Create USERS table
-  DB.create_table? :users do
-    primary_key :id
-    String :username, null: false, unique: true
-    String :password_hash, null: false
-    String :email, null: false, unique: true
-    String :avatar
-    DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
-    DateTime :updated_at, default: Sequel::CURRENT_TIMESTAMP
-  end
-
-  # User model
   class User < Sequel::Model
     plugin :validation_helpers
-
     def validate
       super
       validates_presence [:username, :password_hash, :email]
@@ -75,28 +52,14 @@ class AuthService < Sinatra::Base
       end
     end
   end
-
-  before do
-    puts "Received request: #{request.request_method} #{request.path}"
-  end
   # Register API route
   post '/api/register' do
-    puts "Received request: #{request.request_method} #{request.path}"
     user_data = {
       username: @request_payload[:username],
       password_hash: BCrypt::Password.create(@request_payload[:password]),
       email: @request_payload[:email],
       avatar: @request_payload[:avatar] || '/images/default-avatar.png'
     }
-    # Just testing incoming data
-    puts "Testing input"
-    if user_data[:username].to_s.empty?
-      puts 'empty username field'
-    end
-    puts user_data[:password_hash]
-    puts user_data[:email]
-    puts user_data[:avatar]
-    puts "End of testing input -----"
     user = User.new(user_data)
 
     begin
@@ -105,8 +68,7 @@ class AuthService < Sinatra::Base
         status 201
         { message: 'User registered successfully' }.to_json
       else
-        status 400
-        { errors: user.errors.full_messages }.to_json
+        halt 400, { errors: user.errors.full_messages }.to_json
       end
     rescue Sequel::UniqueConstraintViolation => e
       # Handle database-level uniqueness violations
@@ -120,38 +82,23 @@ class AuthService < Sinatra::Base
   post '/api/login' do
     begin
       content_type :json
-      puts "Processing login request..."
-
       username = @request_payload[:username]
       password = @request_payload[:password]
-      puts "Received username: #{username}"
-      puts "Received password: #{password}"
-
       # Halt if username or password is missing
       halt 400, { error: 'Username and password are required' }.to_json unless username && password
-
       # Fetch the user from the database
       user = User.where(username: username).first
-      puts "Fetched user: #{user.inspect}"
-
       # Validate the password
       if user && BCrypt::Password.new(user.password_hash) == password
-        puts "Password validated successfully"
-        # Generate JWT token
-        #token = generate_jwt({ user_id: user.id, username: user.username })
         token = @jwt_manager.generate_jwt(user_id: user.id, username: user.username)
-        status 200
-        { message: 'Login successful', token: token }.to_json
+        halt 200, { message: 'Login successful', token: token }.to_json
       else
-        puts "Invalid username or password"
-        status 401
-        { error: 'Invalid username or password' }.to_json
+        halt 401, { error: 'Invalid username or password' }.to_json
       end
-    rescue => e
-      puts "An error occurred: #{e.message}"
-      puts e.backtrace.join("\n")
-      status 500
-      { error: 'Internal Server Error' }.to_json
+    rescue Sequel::DatabaseError
+      halt 500, { error: 'Internal Database Error' }.to_json
+    rescue StandardError
+      halt 500, {error: 'Internal Server Error'}.to_json
     end
   end
 
@@ -159,8 +106,6 @@ class AuthService < Sinatra::Base
     authenticate_request!
   end
   get '/api/user' do
-    puts 'Testing user info'
-    puts @current_user[:username]
     halt 401, { error: 'Unauthorized' }.to_json unless @current_user
     user = @current_user
     status 200
--- src/auth_service/db_handler.rb
+++ src/auth_service/db_handler.rb
@@ -8,25 +8,27 @@ class DbHandler
     @db = Sequel.connect(db_connection)
   end
 
-
-  # db_connection = {
-  #   adapter: 'postgres',
-  #   host: 'db-service',
-  #   database: 'auth_db',
-  #   user: 'auth_user',
-  #   password: 'securepassword'
-  # }
-
-  @db.
-  # Create USERS table
-  DB.create_table? :users do
-    primary_key :id
-    String :username, null: false, unique: true
-    String :password_hash, null: false
-    String :email, null: false, unique: true
-    String :avatar
-    DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
-    DateTime :updated_at, default: Sequel::CURRENT_TIMESTAMP
+  def create_table
+    # Create USERS table
+    @db.create_table? :users do
+      primary_key :id
+      String :username, null: false, unique: true
+      String :password_hash, null: false
+      String :email, null: false, unique: true
+      String :avatar
+      DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+      DateTime :updated_at, default: Sequel::CURRENT_TIMESTAMP
+    end
   end
 end
 
+db_connection = {
+  adapter: 'postgres',
+  host: 'db-service',
+  database: 'auth_db',
+  user: 'auth_user',
+  password: 'securepassword'
+}
+
+db = DbHandler.new(db_connection)
+db.create_table
