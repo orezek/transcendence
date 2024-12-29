1a2fa9016106508a58e81b63fd4a9ae506b02061
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-12-29 15:31:44 +0100

    feat(api): add debugger to auth_service

--- src/auth_service/entrypoint.sh
+++ src/auth_service/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /usr/src/app
 # Check if Gemfile exists
 if [ -f "Gemfile" ]; then
   echo "Gemfile detected, installing dependencies..."
@@ -8,5 +9,10 @@ else
   echo "No Gemfile found. Skipping bundle install."
 fi
 
-# Run the main container command
-exec "$@"
+if [ "$PROFILE" = "debug" ]; then
+  echo "Running in debug mode"
+  exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- ./auth_service.rb -o 0.0.0.0
+else
+  echo "Running in normal mode"
+  exec ruby ./auth_service.rb -o 0.0.0.0
+fi
