ee8fdd6185246dc5f6cb823d8ad4a94c56b60e04
O.R <orezek@seznam.cz>
2025-01-11 16:11:55 +0100

    refactor: setting up container structure

--- services/auth_service/app/tests/main.rb
+++ services/auth_service/app/tests/main.rb
@@ -2,19 +2,24 @@
 
 require 'vault'
 
-PASS_TOKEN = 'hvs.CAESIO9ULYvkvpKFnFkGd8qHY0qrYglEWQl8GS2Cnbl1gVRVGh4KHGh2cy44dVlnOXUwMHViTGlxYUlhSHZhbG1YRFI'
-ROOT_TOKEN = 'hvs.j5txn8mxHRmiFYT2XDI9wFHM'
+# PASS_TOKEN = 'hvs.CAESIO9ULYvkvpKFnFkGd8qHY0qrYglEWQl8GS2Cnbl1gVRVGh4KHGh2cy44dVlnOXUwMHViTGlxYUlhSHZhbG1YRFI'
+# ROOT_TOKEN = 'hvs.j5txn8mxHRmiFYT2XDI9wFHM'
+#
+# # Test vault api
+# Vault.configure do |config|
+#   config.address = ENV['VAULT_ADDR'] || 'http://127.0.0.1:8200' || 'http://secrets_service:8200'
+#   config.token = ENV['VAULT_TOKEN'] || PASS_TOKEN
+# end
+#
+# secret = Vault.kv('password_manager').read('jwt_secret_key') #returns Vault object
+# puts secret.inspect
+# puts secret.data[:jwt_secret_key]
+#
+#
+# secret = Vault.kv('password_manager').read('jwt_secret_key').data[:jwt_secret_key]
+# puts secret
 
-# Test vault api
-Vault.configure do |config|
-  config.address = ENV['VAULT_ADDR'] || 'http://127.0.0.1:8200' || 'http://secrets_service:8200'
-  config.token = ENV['VAULT_TOKEN'] || PASS_TOKEN
+
+5.times do |n|
+  puts "hello from #{__FILE__}: #{n}"
 end
-
-secret = Vault.kv('password_manager').read('jwt_secret_key') #returns Vault object
-puts secret.inspect
-puts secret.data[:jwt_secret_key]
-
-
-secret = Vault.kv('password_manager').read('jwt_secret_key').data[:jwt_secret_key]
-puts secret
--- services/status_service/app/status_service.rb
+++ services/status_service/app/status_service.rb
@@ -1,2 +1,10 @@
-require sinatra
+# frozen_string_literal: true
+
+require 'sinatra'
+set :bind, '0.0.0.0' # Bind to all interfaces
+
+get '/' do
+  status 200 # status
+  'Status service'.to_json # response body
+end
 
